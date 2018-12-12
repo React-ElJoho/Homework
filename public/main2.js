34f4a9ec1d150eda4976d8264ecf1fcdc547335b
ElJoho <johalopezari@unal.edu.co>
2018-12-12 15:17:24 -0500

    Hoy unimos el boton al percent en donde, nos arroja un valor que lo unimos a un array y lo guardamos para ser posteriormente organizado.

--- public/main2.js
+++ public/main2.js
@@ -1,12 +1,5 @@
-class Azul{
-	constructor(){
-		//
-	}
-}
 class Task {
 	constructor(root){
-		this.element="hola2"
-		this.amarillo= new Azul()
 		this.contenedorTasks =document.createElement("div")
 		this.contenedorTasks.classList.add("tasks")
 		this.name =document.createElement("h4")
@@ -14,23 +7,18 @@ class Task {
 		this.deadline =new Input("text","deadline", "Hola2")
 		this.percent = new Input("text", "percent","Hola3")
 		this.like =new Input("text","like","Hola4")
-		this.boton = new Input("boton","button","Hola5","Hola5")
+		this.boton = new Input("button","boton","Hola5","Hola5")
 		this.contenedorTasks.appendChild(this.work.getElement())
 		this.contenedorTasks.appendChild(this.percent.getElement())
 		this.contenedorTasks.appendChild(this.like.getElement())
 		this.contenedorTasks.appendChild(this.deadline.getElement())
 		this.contenedorTasks.appendChild(this.boton.getElement())
 		document.getElementById(root).appendChild(this.contenedorTasks)
-		console.log(this)
+		
 		this.onClick= this.onClick.bind(this);// Aca estamos haciendo que el elemento onClick tenga el contexto de esta clase. 
 		this.boton.getElement().addEventListener ("click",this.onClick) // Aca estamos llamando al elemento HTML del Boton y ademas
 		// estamos escuchando cuando suceda el evento click  de ese elemento para ejecutar el metodo onClick. 
 		
-		 this.descriptionTime = `Tiempo que toma hacerlo menos fecha de entrega y da el valor de dias correspondientes`
-		 this.descriptionPercent =` De acuerdo al porcentaje que tiene la tarea asiganara
-		 un valor de puntaje priiorizando el de mayor porcentaje al de menor porcentaje`
-		 this.descriptionLikeability =`Se utiliza como ultimop recurso el gusto que tiene la persona por dicha tarea si sus 
-		 valores han sido iguales en las otras categorias`
 		  this.percentO= this.percentO.bind(this);
 		 this.likeability= this.likeability.bind(this);
 		 this.time= this.time.bind(this);
@@ -38,28 +26,32 @@ class Task {
 	}
 
 	time() {
+
 			//return this.work.getElement().value - this.deadline.getElement().value 
-			console.log(this.descriptionTime)
+			
 	}
 
 	percentO() { 
-			console.log(this.descriptionPercent)
+		console.log(this.percent.getAttribute("value"));// Para llamar al elemento es solo necesario poner el this.percent
+		if (this.percentArray) {
+			console.log("existe")
+
+		} else {
+			this.percentArray = [];
+			console.log("no exisitia")
+
+		} this.percentArray.push(this.percent.getAttribute("value"))
+		console.log(this.percentArray);// Lo que hicimos aca fue añadir un condicional en el cual recibimos cada vez que 
+		//oprimamos el boton y el valor que recibamos de ahi lo guardamos en un arreglo para ser posteriormente organizado. 
 	}
 
 	likeability() {
-			console.log(this.descriptionLikeability)
+			
 	}
 	
 	onClick(){
-		function ol(){
-			this.time();
-			this.percentO();
-			this.likeability();
-
-		}
-
-		ol=ol.bind(this);		
-		ol()
+		this.percentO();//Aca llamamos la funcion dentro del Onclick.
+		
 	}
 
 
@@ -67,20 +59,32 @@ class Task {
 
 }
 class Input {
-	constructor(id,type,placeholder,value=""){
+	constructor(type,id,placeholder,value=""){
 		this.element = document.createElement("input");
 		this.element.placeholder =placeholder;
 		this.element.type = type;
 		this.element.id = id; 
 		this.element.value = value; 
-		
 
 	}
 	getElement(){
 		return this.element;
 	}
-}
+	getAttribute(nameAttribute){
+		if(nameAttribute=="placeholder"){
+			return this.element.placeholder;
+		} else if(nameAttribute=="type"){
+			return this.element.type;
+		} else if(nameAttribute=="id"){
+			return this.element.id;
+		} else if(nameAttribute=="value"){
+			return this.element.value;
+		} else if(nameAttribute=="class"){
+			return this.element.className;
 
+		}else return false;
+	}
+}
 
 
 window.onload = function (){
