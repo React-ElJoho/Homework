490d225f17812cef585eb71da913dc21b3a841fe
SantiagoBotiaC <santiagobotiacella@gmail.com>
2018-12-13 14:35:01 -0500

    creación de la clase percentO que agraga al arreglo percentArray los valores ingresados en el input correspondiente

--- public/main2.js
+++ public/main2.js
@@ -1,6 +1,6 @@
 class Task {
 	constructor(root){
-		this.contenedorTasks =document.createElement("div")
+		this.contenedorTasks = document.createElement("div")
 		this.contenedorTasks.classList.add("tasks")
 		this.name =document.createElement("h4")
 		this.work = new Input("text","work","Hola")
@@ -13,36 +13,43 @@ class Task {
 		this.contenedorTasks.appendChild(this.like.getElement())
 		this.contenedorTasks.appendChild(this.deadline.getElement())
 		this.contenedorTasks.appendChild(this.boton.getElement())
-		document.getElementById(root).appendChild(this.contenedorTasks)
-		
-		this.onClick= this.onClick.bind(this);// Aca estamos haciendo que el elemento onClick tenga el contexto de esta clase. 
-		this.boton.getElement().addEventListener ("click",this.onClick) // Aca estamos llamando al elemento HTML del Boton y ademas
-		// estamos escuchando cuando suceda el evento click  de ese elemento para ejecutar el metodo onClick. 
-		
-		  this.percentO= this.percentO.bind(this);
-		 this.likeability= this.likeability.bind(this);
-		 this.time= this.time.bind(this);
+        document.getElementById(root).appendChild(this.contenedorTasks)
+		this.percentO= this.percentO.bind(this);
+        this.likeability= this.likeability.bind(this);
+        this.time= this.time.bind(this);
+        this.onClick= this.onClick.bind(this);// Aca estamos haciendo que el elemento onClick tenga el contexto de esta clase. 
+        this.boton.getElement().addEventListener ("click",this.onClick) // Aca estamos llamando al elemento HTML del Boton y ademas
 
 	}
 
-	time() {
+//			
+//		console.log(this)
+//		this.onClick= this.onClick.bind(this);// Aca estamos haciendo que el elemento onClick tenga el contexto de esta clase. 
+//		this.boton.getElement().addEventListener ("click",this.onClick) // Aca estamos llamando al elemento HTML del Boton y ademas
+//		// estamos escuchando cuando suceda el evento click  de ese elemento para ejecutar el metodo onClick.     
+//        this.percentO= this.percentO.bind(this);
+//        this.likeability= this.likeability.bind(this);
+//        this.time= this.time.bind(this);
 
-			//return this.work.getElement().value - this.deadline.getElement().value 
+	
+
+	time() {
 			
 	}
 
 	percentO() { 
-		console.log(this.percent.getAttribute("value"));// Para llamar al elemento es solo necesario poner el this.percent
-		if (this.percentArray) {
-			console.log("existe")
-
-		} else {
-			this.percentArray = [];
-			console.log("no exisitia")
-
-		} this.percentArray.push(this.percent.getAttribute("value"))
-		console.log(this.percentArray);// Lo que hicimos aca fue añadir un condicional en el cual recibimos cada vez que 
-		//oprimamos el boton y el valor que recibamos de ahi lo guardamos en un arreglo para ser posteriormente organizado. 
+        //console.log(this.element.getElementById("percent").getAttribute("value"));
+        //únicamente es necesario llamar this.percent.getAttribute.
+        if (this.percentArray){
+            console.log("Ya existe");   
+        } else{
+            this.percentArray = [];
+            console.log("No existía");
+        }
+        console.log(this.percent.getAttribute("value"));
+        this.percentArray.push(this.percent.getAttribute("value"));
+        //Agregamos al arreglo cada una de las variables que se van ingresando en el input de porcentaje
+        console.log(this.percentArray);
 	}
 
 	likeability() {
@@ -50,12 +57,8 @@ class Task {
 	}
 	
 	onClick(){
-		this.percentO();//Aca llamamos la funcion dentro del Onclick.
-		
+		this.percentO();
 	}
-
-
-
 
 }
 class Input {
@@ -64,40 +67,41 @@ class Input {
 		this.element.placeholder =placeholder;
 		this.element.type = type;
 		this.element.id = id; 
-		this.element.value = value; 
+		this.element.value = value;
+		
 
 	}
 	getElement(){
 		return this.element;
 	}
-	getAttribute(nameAttribute){
-		if(nameAttribute=="placeholder"){
-			return this.element.placeholder;
-		} else if(nameAttribute=="type"){
-			return this.element.type;
-		} else if(nameAttribute=="id"){
-			return this.element.id;
-		} else if(nameAttribute=="value"){
-			return this.element.value;
-		} else if(nameAttribute=="class"){
-			return this.element.className;
-
-		}else return false;
-	}
+    
+    getAttribute(nameAttribute){
+        if(nameAttribute == "placeholder"){
+            return this.element.placeholder;
+        }
+        else if(nameAttribute == "type"){
+            return this.element.type;
+        }
+        else if(nameAttribute == "id"){
+            return this.element.id;
+        }
+        else if(nameAttribute == "value"){
+            return this.element.value;
+        }
+        else if(nameAttribute == "class"){
+            return this.element.className;
+        }
+        else return false;
+    }
 }
 
 
 window.onload = function (){
-	window.innerText ="Hola mundo"
-	var jave1 = document.createElement ("div")
+	/*var jave1 = document.createElement ("div")
 	jave1.innerText ="hola mundo"
-	document.getElementById("root").appendChild(jave1)
+	document.getElementById("root").appendChild(jave1)*/
 	const contenedor =new Task ("root")
 	var hola= new Task("root")
 
 
 };
-
-
-
-
